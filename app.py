afb1900318e660196d9c4b06e42378a1330a499f
Savinay Pandey <140955852+SaviPandey@users.noreply.github.com>
2025-12-27 05:25:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from waitress import serve
 
 mode = os.getenv('MODE', 'dev')
 
-# if __name__ == '__main__':
-#     if mode == 'dev':
-#         app.run(host='0.0.0.0', port=5000, debug=True)
-#     else:
-#         serve(app, host='0.0.0.0', port=8000, threads=4)
+if __name__ == '__main__':
+    if mode == 'dev':
+        app.run(host='0.0.0.0', port=5000, debug=True)
+    else:
+        serve(app, host='0.0.0.0', port=8000, threads=4)
 
