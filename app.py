047b994777b08eff0c0267da2033ff84ec2f302a
Savinay Pandey <140955852+SaviPandey@users.noreply.github.com>
2025-12-27 10:42:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ mode = os.getenv('MODE', 'dev')
 
 if __name__ == '__main__':
     if mode == 'dev':
-        app.run(host='0.0.0.0', port=5000, debug=True)
+        # app.run(host='0.0.0.0', port=5000, debug=True)
     else:
         serve(app, host='0.0.0.0', port=8000, threads=4)
 
